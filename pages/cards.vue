a9506da3b307f6736492d3f95471a4bc8bfcf483
Yaroslav <yaroyay@gmail.com>
2020-03-01 21:10:24 +0200

    add card template

--- pages/cards.vue
+++ pages/cards.vue
@@ -1,36 +1,38 @@
 <template>
-  <div class="container card-container">
-    <div v-for="card in 5" :key="card" class="card" style="width: 18rem;">
-      <img src="https://images.unsplash.com/photo-1500856311637-fc0249e33e4c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" class="card-img-top" alt="">
-      <div class="card-body">
-        <h5 class="card-title">
-          {{ card }}
-        </h5>
-        <p class="card-text">
-          Some quick example text to build on the card title and make up the bulk of the card's content.
-        </p>
-        <a href="#" class="btn btn-primary">Go somewhere</a>
+  <section>
+    <h1>Card Store</h1>
+    <div class="container card-container">
+      <div v-for="card in 6" :key="card" class="card" style="width: 18rem;">
+        <img
+          src="https://images.unsplash.com/photo-1500856311637-fc0249e33e4c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
+          class="card-img-top"
+          alt
+        />
+        <div class="card-body">
+          <h5 class="card-title">{{ card }}</h5>
+          <p
+            class="card-text"
+          >Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
       </div>
     </div>
-  </div>
+  </section>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
 <style scoped>
- .card-container {
-   display: flex;
-   justify-content: center;
-   flex-wrap: wrap;
-   margin-top: 30px;
- }
+.card-container {
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+  margin-top: 30px;
+}
 
- .card {
-   margin: 15px;
- }
-
+.card {
+  margin: 15px;
+}
 </style>
