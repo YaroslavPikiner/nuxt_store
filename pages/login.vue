9a02f703ab2a221904c4159092b2c85ad090ca05
Yaroslav <yaroyay@gmail.com>
2020-03-01 13:25:38 +0200

    add login template

---
+++ pages/login.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="container">
+    <form>
+      <div class="form-group">
+        <label for="exampleInputEmail1">Email address</label>
+        <input type="email" class="form-control" aria-describedby="emailHelp">
+        <small class="form-text text-muted">We'll never share your email with anyone else.</small>
+      </div>
+      <button type="submit" class="btn btn-primary">
+        Submit
+      </button>
+    </form>
+  </div>
+</template>
