bb0bbd5062fb69d58fb5103b7994904a82acfffd
Yaroslav <yaroyay@gmail.com>
2020-03-01 21:19:18 +0200

    fix lint

--- pages/cards.vue
+++ pages/cards.vue
@@ -1,19 +1,28 @@
 <template>
-  <section>
+  <section class="container">
     <h1>Card Store</h1>
-    <div class="container card-container">
+    <div class="card-container">
       <div v-for="card in 6" :key="card" class="card" style="width: 18rem;">
         <img
           src="https://images.unsplash.com/photo-1500856311637-fc0249e33e4c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
           class="card-img-top"
           alt
-        />
+        >
         <div class="card-body">
-          <h5 class="card-title">{{ card }}</h5>
+          <h5 class="card-title">
+            {{ card }}
+          </h5>
           <p
             class="card-text"
-          >Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-          <a href="#" class="btn btn-primary">Go somewhere</a>
+          >
+            Some quick example text to build on the card title and make up the bulk of the card's content.
+          </p>
+          <a
+            href="#"
+            class="btn btn-primary"
+          >
+            Go somewhere
+          </a>
         </div>
       </div>
     </div>
@@ -21,7 +30,7 @@
 </template>
 
 <script>
-export default {};
+export default {}
 </script>
 
 <style scoped>
@@ -30,6 +39,12 @@ export default {};
   justify-content: center;
   flex-wrap: wrap;
   margin-top: 30px;
+}
+img {
+  transition: all 0.3s;
+}
+img:hover {
+  transform: scale(1.05);
 }
 
 .card {
