13b64e6d3a2048ff5b7afe03d5c7c57785107b57
Yaroslav <yaroyay@gmail.com>
2020-03-05 12:39:56 +0200

    styled card id links

--- pages/cards/_id.vue
+++ pages/cards/_id.vue
@@ -1,14 +1,43 @@
 <template>
-  <section class="container">
-    <h1> {{ card.title }} </h1>
+  <section class="container id-cards">
+    <h1> {{ cards.title }} </h1>
+    <img :src="images.url" alt="">
+    <p class="id-body"> {{ cards.body }} </p>
   </section>
 </template>
 
 <script>
 export default {
   async asyncData ({ $axios, params }) {
-    const card = await $axios.$get('https://jsonplaceholder.typicode.com/posts/' + params.id)
-    return { card }
+    const cards = await $axios.$get('https://jsonplaceholder.typicode.com/posts/' + params.id)
+    const images = await $axios.$get('https://jsonplaceholder.typicode.com/photos/' + params.id)
+    return { cards, images }
   }
 }
 </script>
+
+<style>
+
+.id-cards {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+
+.id-cards > h1 {
+  margin-bottom: 40px;
+}
+
+.id-cards > h1::first-letter {
+  text-transform: uppercase;
+}
+
+.id-cards > p::first-letter {
+  text-transform: uppercase;
+}
+
+.id-cards > img {
+  margin-bottom: 40px;
+}
+</style>
