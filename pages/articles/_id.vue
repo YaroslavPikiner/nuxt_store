059e97585997703d94ece86244c58cfb6deb41a1
Yaroslav <yaroyay@gmail.com>
2020-03-05 12:37:52 +0200

    styled id articles link

--- pages/articles/_id.vue
+++ pages/articles/_id.vue
@@ -1,6 +1,8 @@
 <template>
-  <section class="container">
+  <section class="container id-article">
     <h1> {{ articles.title }} </h1>
+    <img :src="images.url" alt="">
+    <p class="id-body"> {{ articles.body }} </p>
   </section>
 </template>
 
@@ -9,8 +11,33 @@ export default {
   async asyncData ({ $axios, params }) {
     const articles = await $axios.$get('https://jsonplaceholder.typicode.com/posts/' + params.id)
     const images = await $axios.$get('https://jsonplaceholder.typicode.com/photos/' + params.id)
-    const users = await $axios.$get('https://jsonplaceholder.typicode.com/users' + params.id)
-    return { articles, images, users }
+    return { articles, images }
   }
 }
 </script>
+
+<style>
+
+.id-article {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+
+.id-article > h1 {
+  margin-bottom: 40px;
+}
+
+.id-article > h1::first-letter {
+  text-transform: uppercase;
+}
+
+.id-article > p::first-letter {
+  text-transform: uppercase;
+}
+
+.id-article > img {
+  margin-bottom: 40px;
+}
+</style>
