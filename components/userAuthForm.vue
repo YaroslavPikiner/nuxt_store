4aec106811802a3184a3707b06b2dac655db4a47
Artur <artur@nextfitness.ai>
2020-03-10 20:47:07 +0200

    add auth validations

--- components/userAuthForm.vue
+++ components/userAuthForm.vue
@@ -1,13 +1,50 @@
 <template>
-    
+  <v-form v-model="valid">
+    <v-text-field
+      v-if="hasName"
+      v-model="userInfo.name"
+      label="Name"
+      :rules="[required('name')]"
+    />
+
+    <v-text-field
+      v-model="userInfo.email"
+      label="Email"
+      :rules="[required('email'), emailFormat()]"
+    />
+
+    <v-text-field
+      v-model="userInfo.password"
+      label="Password"
+      :type="showPassword ? 'text' : 'password'"
+      :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
+      counter="true"
+      :rules="[required('password'), minLength('password', 8)]"
+      @click:append="showPassword = !showPassword"
+    />
+
+    <v-btn :disabled="!valid" @click="submitForm(userInfo)">
+      {{ buttonText }}
+    </v-btn>
+  </v-form>
 </template>
 
 <script>
+import validations from '@/utils/validations.js'
+
 export default {
-  
+  props: ['submitForm', 'buttonText'],
+  data () {
+    return {
+      valid: false,
+      showPassword: false,
+      hasName: false,
+      userInfo: {
+        email: '',
+        password: ''
+      },
+      ...validations
+    }
+  }
 }
 </script>
-
-<style>
-
-</style>
--- pages/login.vue
+++ pages/login.vue
@@ -1,8 +1,24 @@
 <template>
   <div class="container login">
-      <h1> Login </h1>
+    <h1> Login </h1>
+    <UserAuthForm buttonText="Login" :submitForm="loginUser" />
   </div>
 </template>
+
+<script>
+import UserAuthForm from '@/components/UserAuthForm.vue'
+
+export default {
+  components: {
+    UserAuthForm
+  },
+  methods: {
+    loginUser () {
+      alert('You are click')
+    }
+  }
+}
+</script>
 
 <style>
  .login {
