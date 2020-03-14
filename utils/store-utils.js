d25bcd297562cbe8660d994c9b38e4662daa8f55
Yaroslav <yaroyay@gmail.com>
2020-03-14 14:53:33 +0200

    fix files

--- utils/store-utils.js
+++
@@ -1,8 +0,0 @@
-  /* eslint-disable */
-export const getData = async function(url, axios) {
-  let response = await axios.get(url)
-  return {
-    data: response.data.data,
-    included: response.data.included
-  }
-}
